97e670cc5c3e4a39b884eaaea66bf3ff2c8709b8
sanghyun1128 <sanghyun1128@gmail.com>
2025-07-25 13:45:37 +0900

    Complete

---
+++ baekjoon/Javascript/1. Bronze/수정렬하기.js
@@ -0,0 +1,7 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const N = Number(input[0]);
+const numbers = input.slice(1).map(Number);
+
+numbers.sort((a, b) => a - b);
+console.log(numbers.join('\n'));
