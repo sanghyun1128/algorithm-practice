d50117e8443e78105f776cf440f116c2dcb5fd44
sanghyun1128 <sanghyun1128@gmail.com>
2025-07-25 13:45:11 +0900

    Add input file for Baekjoon problem testing in JavaScript

--- baekjoon/Javascript/test.js
+++ baekjoon/Javascript/test.js
@@ -3,3 +3,10 @@
  * This file is used to run tests for various problem solutions.
  */
 
+// Setup for reading input for Baekjoon problems
+// const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+// Setup for reading input from a local file for testing purposes
+const input = require('fs').readFileSync(__dirname + '/input.txt').toString().trim().split('\n');
+
+console.log("Input received for testing:", input);
