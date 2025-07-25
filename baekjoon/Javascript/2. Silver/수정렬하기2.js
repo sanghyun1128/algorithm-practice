060f1c535bcd2623d119450d1dc31e383d129eef
sanghyun1128 <sanghyun1128@gmail.com>
2025-07-25 14:11:56 +0900

    Complete

---
+++ baekjoon/Javascript/2. Silver/수정렬하기2.js
@@ -0,0 +1,8 @@
+// const input = require('fs').readFileSync(__dirname + '/input.txt').toString().trim().split('\n');
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const numberList = input.slice(1).map(Number);;
+
+numberList.sort((a, b) => a - b);
+
+console.log(numberList.join('\n'));
